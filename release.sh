f63b8263c0aca2cd77806debb16112cd691814f2
jon <wangzhong.zw@alibaba-inc.com>
2020-09-09 22:59:31 +0800

    other: update build & release script, add md5sum

--- release.sh
+++ release.sh
@@ -7,10 +7,12 @@ echo "Build release branch origin/release/hive-1.x"
 git checkout --quiet release/hive-1.x
 sh build.sh
 mv mma.tar.gz mma-hive-1.x-release.tar.gz
+mv mma.tar.gz.md5sum mma-hive-1.x-release.tar.gz.md5sum
 echo "Done"
 
 echo "Build release branch origin/release/hive-2.x"
 git checkout --quiet release/hive-2.x
 sh build.sh
 mv mma.tar.gz mma-hive-2.x-release.tar.gz
+mv mma.tar.gz.md5sum mma-hive-2.x-release.tar.gz.md5sum
 echo "Done"
