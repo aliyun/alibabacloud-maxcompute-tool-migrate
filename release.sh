39adc273d0afb1baab99c67a578a33fd938d78f6
jon <wangzhong.zw@alibaba-inc.com>
2020-09-09 17:14:06 +0800

    other: update release script

--- release.sh
+++ release.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "Release starts"
 
-echo "Build release branch release/hive-1.x"
-git checkout --quiet release/hive-1.x
+echo "Build release branch origin/release/hive-1.x"
+git checkout --quiet origin/release/hive-1.x
 sh build.sh
 mv mma.tar.gz mma-hive-1.x-release.tar.gz
 echo "Done"
 
-echo "Build release branch release/hive-2.x"
-git checkout --quiet release/hive-2.x
+echo "Build release branch origin/release/hive-2.x"
+git checkout --quiet origin/release/hive-2.x
 sh build.sh
 mv mma.tar.gz mma-hive-2.x-release.tar.gz
 echo "Done"
