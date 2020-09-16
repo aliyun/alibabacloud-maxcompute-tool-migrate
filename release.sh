afa9579ec78fe4dc0955cb0358b967cc23be2da5
jon <wangzhong.zw@alibaba-inc.com>
2020-09-16 16:17:33 +0800

    other: update release script

--- release.sh
+++ release.sh
@@ -3,16 +3,22 @@ set -e
 
 echo "Release starts"
 
-echo "Build release branch origin/release/hive-1.x"
-git checkout --quiet release/hive-1.x
-sh build.sh
-mv mma.tar.gz mma-hive-1.x-release.tar.gz
-mv mma.tar.gz.md5sum mma-hive-1.x-release.tar.gz.md5sum
-echo "Done"
+if [ "" = "${TRAVIS_TAG}" ];
+then
+  echo "Not a release"
+  ./build.sh
+else
+  echo "Build release branch origin/release/hive-1.x"
+  git checkout --quiet release/hive-1.x
+  ./build.sh
+  mv mma.tar.gz mma-hive-1.x-release.tar.gz
+  mv mma.tar.gz.md5sum mma-hive-1.x-release.tar.gz.md5sum
+  echo "Done"
 
-echo "Build release branch origin/release/hive-2.x"
-git checkout --quiet release/hive-2.x
-sh build.sh
-mv mma.tar.gz mma-hive-2.x-release.tar.gz
-mv mma.tar.gz.md5sum mma-hive-2.x-release.tar.gz.md5sum
-echo "Done"
+  echo "Build release branch origin/release/hive-2.x"
+  git checkout --quiet release/hive-2.x
+  ./build.sh
+  mv mma.tar.gz mma-hive-2.x-release.tar.gz
+  mv mma.tar.gz.md5sum mma-hive-2.x-release.tar.gz.md5sum
+  echo "Done"
+fi
