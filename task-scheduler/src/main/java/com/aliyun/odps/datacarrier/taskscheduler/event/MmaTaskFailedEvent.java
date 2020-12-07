cba0fd82eaa1d3222ce97da5346b5dd70cd3eb64
jon <wangzhong.zw@alibaba-inc.com>
2020-12-07 22:31:44 +0800

    opt: refactor MMA events

---
+++ task-scheduler/src/main/java/com/aliyun/odps/datacarrier/taskscheduler/event/MmaTaskFailedEvent.java
@@ -0,0 +1,30 @@
+package com.aliyun.odps.datacarrier.taskscheduler.event;
+
+import java.util.List;
+import java.util.Objects;
+
+public class MmaTaskFailedEvent extends BaseMmaEvent {
+
+  private String id;
+  private List<String> actionIds;
+
+  public MmaTaskFailedEvent(String id, List<String> actionIds) {
+    this.id = Objects.requireNonNull(id);
+    this.actionIds = actionIds;
+  }
+
+  @Override
+  public MmaEventType getType() {
+    return MmaEventType.JOB_FAILED;
+  }
+
+  @Override
+  public String toString() {
+    if (actionIds != null) {
+      return String.format(
+          "Task failed: %s (caused by %s)", id, String.join(", ", actionIds));
+    } else {
+      return String.format("Task failed: %s", id);
+    }
+  }
+}
