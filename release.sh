a6b8690c7b7eb76f77b1fb12a4ac80befff8dd7b
Zhong Wang <wangzhong.zw@alibaba-inc.com>
2021-08-16 16:29:15 +0800

    Merge pull request #40 from aliyun/opt/mvn_assembly
    opt: add maven assemble plugin

--- release.sh
+++ release.sh
@@ -3,26 +3,23 @@ set -e
 
 echo "Release starts"
 
-if [ "" = "${TRAVIS_TAG}" ];
-then
-  echo "Not a release"
-  ./build.sh
-else
-  echo "Build release branch origin/release/hive-1.x"
-  git checkout --quiet release/hive-1.x
-  ./build.sh
-  mv mma.tar.gz mma-hive-1.x-release.tar.gz
-  echo "Done"
+MMA_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Version: ${MMA_VERSION}"
 
-  echo "Build release branch origin/release/hive-2.x"
-  git checkout --quiet release/hive-2.x
-  ./build.sh
-  mv mma.tar.gz mma-hive-2.x-release.tar.gz
-  echo "Done"
+echo "Build release branch origin/release/hive-1.x"
+git checkout --quiet release/hive-1.x
+mvn -U -q clean package -DskipTests
+mv distribution/target/mma-${MMA_VERSION}.zip mma-${MMA_VERSION}-hive-1.x.zip
+echo "Done"
 
-  echo "Build release branch origin/release/hive-3.x"
-  git checkout --quiet release/hive-3.x
-  ./build.sh
-  mv mma.tar.gz mma-hive-3.x-release.tar.gz
-  echo "Done"
-fi
+echo "Build release branch origin/release/hive-2.x"
+git checkout --quiet release/hive-2.x
+mvn -U -q clean package -DskipTests
+mv distribution/target/mma-${MMA_VERSION}.zip mma-${MMA_VERSION}-hive-2.x.zip
+echo "Done"
+
+echo "Build release branch origin/release/hive-3.x"
+git checkout --quiet release/hive-3.x
+mvn -U -q clean package -DskipTests
+mv distribution/target/mma-${MMA_VERSION}.zip mma-${MMA_VERSION}-hive-3.x.zip
+echo "Done"
