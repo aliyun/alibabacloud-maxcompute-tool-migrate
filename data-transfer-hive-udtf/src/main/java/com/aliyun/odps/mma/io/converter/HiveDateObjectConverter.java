2152508e499fb6438bffbb7ccf1d686bc270f367
wuyue.lyd <wuyue.lyd@alibaba-inc.com>
2022-02-09 16:07:15 +0800

    feature: use reflect to handle different hive version in udtf

--- data-transfer-hive-udtf/src/main/java/com/aliyun/odps/mma/io/converter/HiveDateObjectConverter.java
+++ data-transfer-hive-udtf/src/main/java/com/aliyun/odps/mma/io/converter/HiveDateObjectConverter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2021 Alibaba Group Holding Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,10 +16,15 @@
 
 package com.aliyun.odps.mma.io.converter;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Date;
+
 import com.aliyun.odps.OdpsType;
 import com.aliyun.odps.type.TypeInfo;
+
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.DateObjectInspector;
+import org.apache.hive.common.util.HiveVersionInfo;
 
 public class HiveDateObjectConverter extends AbstractHiveObjectConverter {
 
@@ -29,8 +34,25 @@ public class HiveDateObjectConverter extends AbstractHiveObjectConverter {
       return null;
     }
 
-    DateObjectInspector dateObjectInspector = (DateObjectInspector) objectInspector;
-    java.sql.Date value = dateObjectInspector.getPrimitiveJavaObject(o);
+    java.sql.Date value;
+    try {
+      Class<?> dateInspectorClazz = Class.forName(
+          "org.apache.hadoop.hive.serde2.objectinspector.primitive.DateObjectInspector");
+      Method getPrimitiveJavaObject = dateInspectorClazz
+          .getDeclaredMethod("getPrimitiveJavaObject", Object.class);
+      Object date = getPrimitiveJavaObject.invoke(dateInspectorClazz.cast(objectInspector), o);
+      String version = HiveVersionInfo.getVersion();
+      if (version.startsWith("3")) {
+        Class<?> hiveDateClazz = Class.forName("org.apache.hadoop.hive.common.type.Date");
+        Method toEpochMilli = hiveDateClazz.getDeclaredMethod("toEpochMilli");
+        value = new Date((Long) toEpochMilli.invoke(date));
+      } else {
+        value = (Date) date;
+      }
+    } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+
     if (OdpsType.STRING.equals(odpsTypeInfo.getOdpsType())) {
       return value.toString();
     } else if (OdpsType.DATETIME.equals(odpsTypeInfo.getOdpsType())) {
