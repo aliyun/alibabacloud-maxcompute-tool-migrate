d4156cb80751e2e5c73b9bd50b15c96215443952
wuyue.lyd <wuyue.lyd@alibaba-inc.com>
2022-02-09 16:07:15 +0800

    feature: use reflect to handle different hive version in udtf

--- data-transfer-hive-udtf/src/main/java/com/aliyun/odps/mma/io/converter/HiveTimeStampObjectConverter.java
+++ data-transfer-hive-udtf/src/main/java/com/aliyun/odps/mma/io/converter/HiveTimeStampObjectConverter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2021 Alibaba Group Holding Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,14 @@
 
 package com.aliyun.odps.mma.io.converter;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Timestamp;
+
 import com.aliyun.odps.type.TypeInfo;
+
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.TimestampObjectInspector;
+import org.apache.hive.common.util.HiveVersionInfo;
 
 public class HiveTimeStampObjectConverter extends AbstractHiveObjectConverter {
 
@@ -28,7 +33,24 @@ public class HiveTimeStampObjectConverter extends AbstractHiveObjectConverter {
       return null;
     }
 
-    TimestampObjectInspector timestampObjectInspector = (TimestampObjectInspector) objectInspector;
-    return timestampObjectInspector.getPrimitiveJavaObject(o);
+    try {
+      Class<?> timestampInspectorClazz = Class.forName(
+          "org.apache.hadoop.hive.serde2.objectinspector.primitive.TimestampObjectInspector");
+      Method getPrimitiveJavaObject = timestampInspectorClazz
+          .getDeclaredMethod("getPrimitiveJavaObject", Object.class);
+      Object timestamp = getPrimitiveJavaObject.invoke(timestampInspectorClazz.cast(objectInspector), o);
+      if (HiveVersionInfo.getVersion().startsWith("3")) {
+        Class<?> hiveTimestampClazz = Class.forName("org.apache.hadoop.hive.common.type.Timestamp");
+        Method toEpochMilli = hiveTimestampClazz.getDeclaredMethod("toEpochMilli");
+        Method getNanos = hiveTimestampClazz.getDeclaredMethod("getNanos");
+        Timestamp t = new Timestamp((Long) toEpochMilli.invoke(timestamp));
+        t.setNanos((Integer) getNanos.invoke(timestamp));
+        return t;
+      } else {
+        return timestamp;
+      }
+    } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
