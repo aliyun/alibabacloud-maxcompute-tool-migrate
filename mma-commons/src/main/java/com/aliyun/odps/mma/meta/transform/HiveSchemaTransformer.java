d483567a45d972a452a826a0fc67c23ec647788a
舍然 <liukaizeng.lkz@alibaba-inc.com>
2022-04-08 14:29:07 +0800

    fix: 分区类型为date时，mc创建table时会有重复的分区字段

--- mma-commons/src/main/java/com/aliyun/odps/mma/meta/transform/HiveSchemaTransformer.java
+++ mma-commons/src/main/java/com/aliyun/odps/mma/meta/transform/HiveSchemaTransformer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2021 Alibaba Group Holding Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,54 +26,55 @@ import com.aliyun.odps.mma.meta.MetaSource.TableMetaModel.TableMetaModelBuilder;
 
 public class HiveSchemaTransformer implements SchemaTransformer {
 
-  private TypeTransformer typeTransformer = new HiveTypeTransformer();
+    private TypeTransformer typeTransformer = new HiveTypeTransformer();
 
-  @Override
-  public SchemaTransformResult transform(TableMetaModel source, JobConfiguration config) {
-    List<TypeTransformResult> typeTransformResults = new ArrayList<>();
-    List<ColumnMetaModel> mcColumnMetaModels = new ArrayList<>(source.getColumns().size());
+    @Override
+    public SchemaTransformResult transform(TableMetaModel source, JobConfiguration config) {
+        List<TypeTransformResult> typeTransformResults = new ArrayList<>();
+        List<ColumnMetaModel> mcColumnMetaModels = new ArrayList<>(source.getColumns().size());
 
-    for (ColumnMetaModel hiveColumnMetaModel : source.getColumns()) {
-      // TODO: to MC type v1
-      TypeTransformResult typeTransformResult = typeTransformer.toMcTypeV2(
-          hiveColumnMetaModel.getColumnName(), hiveColumnMetaModel.getType());
-      typeTransformResults.add(typeTransformResult);
-      mcColumnMetaModels.add(
-          new ColumnMetaModel(
-              hiveColumnMetaModel.getColumnName(),
-              typeTransformResult.getTransformedType(),
-              hiveColumnMetaModel.getComment()));
-    }
-
-    TableMetaModelBuilder builder = new TableMetaModelBuilder(
-        config.getOrDefault(JobConfiguration.DEST_CATALOG_NAME, source.getDatabase()),
-        config.getOrDefault(JobConfiguration.DEST_OBJECT_NAME, source.getTable()),
-        mcColumnMetaModels);
-
-    if (!source.getPartitionColumns().isEmpty()) {
-      List<ColumnMetaModel> mcPartitionColumnMetaModels =
-          new ArrayList<>(source.getPartitionColumns().size());
-      for (ColumnMetaModel hivePartitionColumnMetaModel : source.getPartitionColumns()) {
-        if ("DATE".equalsIgnoreCase(hivePartitionColumnMetaModel.getType())) {
-          mcPartitionColumnMetaModels.add(
-              new ColumnMetaModel(
-                  hivePartitionColumnMetaModel.getColumnName(),
-                  "STRING",
-                  hivePartitionColumnMetaModel.getComment()));
+        for (ColumnMetaModel hiveColumnMetaModel : source.getColumns()) {
+            // TODO: to MC type v1
+            TypeTransformResult typeTransformResult = typeTransformer.toMcTypeV2(
+                    hiveColumnMetaModel.getColumnName(), hiveColumnMetaModel.getType());
+            typeTransformResults.add(typeTransformResult);
+            mcColumnMetaModels.add(
+                    new ColumnMetaModel(
+                            hiveColumnMetaModel.getColumnName(),
+                            typeTransformResult.getTransformedType(),
+                            hiveColumnMetaModel.getComment()));
         }
-        TypeTransformResult typeTransformResult = typeTransformer.toMcTypeV2(
-            hivePartitionColumnMetaModel.getColumnName(),
-            hivePartitionColumnMetaModel.getType());
-        typeTransformResults.add(typeTransformResult);
-        mcPartitionColumnMetaModels.add(
-            new ColumnMetaModel(
-                hivePartitionColumnMetaModel.getColumnName(),
-                typeTransformResult.getTransformedType(),
-                hivePartitionColumnMetaModel.getComment()));
-      }
-      builder.partitionColumns(mcPartitionColumnMetaModels);
-      builder.partitions(source.getPartitions());
+
+        TableMetaModelBuilder builder = new TableMetaModelBuilder(
+                config.getOrDefault(JobConfiguration.DEST_CATALOG_NAME, source.getDatabase()),
+                config.getOrDefault(JobConfiguration.DEST_OBJECT_NAME, source.getTable()),
+                mcColumnMetaModels);
+
+        if (!source.getPartitionColumns().isEmpty()) {
+            List<ColumnMetaModel> mcPartitionColumnMetaModels =
+                    new ArrayList<>(source.getPartitionColumns().size());
+            for (ColumnMetaModel hivePartitionColumnMetaModel : source.getPartitionColumns()) {
+                if ("DATE".equalsIgnoreCase(hivePartitionColumnMetaModel.getType())) {
+                    mcPartitionColumnMetaModels.add(
+                            new ColumnMetaModel(
+                                    hivePartitionColumnMetaModel.getColumnName(),
+                                    "STRING",
+                                    hivePartitionColumnMetaModel.getComment()));
+                } else {
+                    TypeTransformResult typeTransformResult = typeTransformer.toMcTypeV2(
+                            hivePartitionColumnMetaModel.getColumnName(),
+                            hivePartitionColumnMetaModel.getType());
+                    typeTransformResults.add(typeTransformResult);
+                    mcPartitionColumnMetaModels.add(
+                            new ColumnMetaModel(
+                                    hivePartitionColumnMetaModel.getColumnName(),
+                                    typeTransformResult.getTransformedType(),
+                                    hivePartitionColumnMetaModel.getComment()));
+                }
+            }
+            builder.partitionColumns(mcPartitionColumnMetaModels);
+            builder.partitions(source.getPartitions());
+        }
+        return new SchemaTransformResult(builder.build(), typeTransformResults);
     }
-    return new SchemaTransformResult(builder.build(), typeTransformResults);
-  }
 }
